d26389500793d35089d800f25dd5ceb5c24d07e2
ITOTDEL4U <woodstore4u@gmail.com>
2022-11-07 01:57:22 +0200

    revision fix

--- Cache.go
+++ Cache.go
@@ -1,32 +1,74 @@
 package cache
 
-// Создаю типd
-// (наподобие справочник например контрагенты. у которго есть своя таблица)
-type cache struct {
-	table map[string]int
+import "fmt"
+
+// I bind a function to a type (for example, as in an object module)
+// The function returns a reference to the OBJECT, not the value itself.
+// & - GetLink()
+// * - GetObject()
+
+//-------------------- Error unit--------
+type appError struct {
+	Err       error
+	TextError string
+}
+type AppError interface {
+	Error() string
+	Unwrap() error
 }
 
-// привязываю функцию к типу (например как в модуле объекта)
-// функция возвращает ссылку на обЪЕКТ а не само значение.
-// по этому вначале создам контрагента а потом уже верну ссылку, при этом сама таблица
-// должна быть хоть чем то заполнена За меня это делал конфиг. а здесь явно
-// & - Это как ФУНКЦИЯ платформы ПолучитьСсылку()
-// * - это как ФУНКЦИЯ ПолучитьОбъект()
+func Error(e *appError) string {
+	return e.TextError
+}
+func Unwrap(e *appError) error {
+	return e.Err
+}
+
+//----------------------------------------
+
+type cache struct {
+	table map[string]interface{}
+}
 
 func New() *cache {
-	var c cache
-	c.table = make(map[string]int)
-	return &c
+
+	return &cache{table: make(map[string]interface{})}
 }
 
-func (c *cache) Set(s string, val int) {
+func (c *cache) Set(s string, val interface{}) *appError {
+	// in set we don't insert double of key
+	_, exists := c.table[s]
+	if exists {
+		return &appError{
+			Err:       fmt.Errorf("internal error"),
+			TextError: "Existing key is used"}
+	}
+
 	c.table[s] = val
+	return nil
 }
 
-func (c *cache) Get(s string) int {
-	return c.table[s]
+func (c *cache) Get(s string) (interface{}, *appError) {
+	// in get we don't get empty key
+	item, exists := c.table[s]
+	if exists {
+		return item, nil
+	}
+
+	return nil, &appError{
+		Err:       fmt.Errorf("internal error"),
+		TextError: "Key does not exist"}
 }
 
-func (c *cache) Delete(s string) {
-	delete(c.table, s)
+func (c *cache) Delete(s string) *appError {
+	// in Delete we don't Delete empty key
+	_, exists := c.table[s]
+	if exists {
+		delete(c.table, s)
+		return nil
+	}
+	return &appError{
+		Err:       fmt.Errorf("internal error"),
+		TextError: "Key does not exist"}
+
 }
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Cache
+This little library knows how to store values ​​in a map
+
+example: 
+    c:= cache.New()
+    err = c.Set("ID",1)
+    value, err = c.Get("ID")
+    err = c.Delete("ID")
+
+    if err != nil{
+     fmt.Println(err.Error())   
+     fmt.Println(err.Unwrap())
+    }
